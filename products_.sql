32fb0168e43136627dd97d5d21b7322e769fd34d
kristina <kristina-danilova1994@mail.ru>
2020-07-29 23:10:16 +0300

    База данных для задания из урока 5

---
+++ products_.sql
@@ -0,0 +1,49 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: products
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `storehouses_products`
+--
+
+DROP TABLE IF EXISTS `storehouses_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `storehouses_products` (
+  `value_` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `storehouses_products`
+--
+
+LOCK TABLES `storehouses_products` WRITE;
+/*!40000 ALTER TABLE `storehouses_products` DISABLE KEYS */;
+INSERT INTO `storehouses_products` VALUES (15),(82),(44),(97),(90),(89),(41),(9),(49),(30),(68),(99),(0),(49),(94),(8),(92),(7),(70),(76),(63),(7),(27),(56),(29),(45),(55),(4),(94),(4),(9),(18),(63),(13),(95),(0),(93),(83),(59),(24),(19),(7),(59),(94),(54),(95),(9),(84),(12),(17),(93),(52),(34),(32),(45),(39),(5),(89),(97),(52),(17),(18),(92),(74),(22),(23),(27),(63),(52),(97),(57),(77),(31),(48),(23),(52),(57),(62),(55),(66),(55),(58),(82),(83),(89),(30),(58),(31),(40),(6),(23),(63),(78),(9),(87),(25),(2),(10),(59),(42);
+/*!40000 ALTER TABLE `storehouses_products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-29 22:59:05
